ec67a2203afbfea15f1a6db7df4805755a433927
faiyazAziz <106823454+faiyazAziz@users.noreply.github.com>
2025-02-18 14:24:37 +0530

    Update settings.py

--- backend/quiz_system/settings.py
+++ backend/quiz_system/settings.py
@@ -80,6 +80,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://sparklquiz.onrender.com",
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
